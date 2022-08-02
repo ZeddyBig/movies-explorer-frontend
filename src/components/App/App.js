a3c9f0b11d62a72b0cad34afb551a7207c0cca0a
Yuri Chetverikov <bashered@gmail.com>
2022-08-02 20:25:11 +0300

    add: Добавить структуру проекта и header

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+// import { Route, Routes } from 'react-router-dom';
+import Main from '../Main/Main';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Common/Header/Header.js
@@ -0,0 +1,22 @@
+import logo from '../../../images/logo.svg';
+
+function Header() {
+    return (
+        <header className="Header">
+            <a href="!" className="header__logo">
+                <img src={logo} alt="Логотип" className="header__logo-image"/>
+            </a>
+            <div className="header__movies_nav"> {/* Сделай списком и выровняй всё по центру относительно страницы */}
+                <a href="!" className="header__movies">Фильмы</a>
+                <a href="!" className="header__movies">Сохранённые фильмы</a>
+            </div>
+            <div className="header__signin-signup">
+                <a href="!" className="header__signup">Регистрация</a>
+                <a href="!" className="header__signin">Войти</a>
+            </div>
+            <a href="!" className="header__account">Аккаунт</a>
+        </header>
+    )
+}
+
+export default Header;
