8d99a9c6e180bd6d773912094a2286a0d2f3c40c
Yuri Chetverikov <bashered@gmail.com>
2022-08-07 12:05:30 +0300

    add: Добавить валидацию для регистрации

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -2,43 +2,98 @@ import React, { Component } from 'react';
 import logo from '../../images/logo.svg';
 import { Link } from 'react-router-dom';
 
-function Register() {
+export default class Register extends Component {
+    state = {
+        fields: {},
+        fieldErrors: {}
+    };
 
-    return (
-        <section className="register">
-            <Link to={"/"} className="register__logo">
-                <img src={logo} alt="Логотип" className="register__logo-image"/>
-            </Link>
-            <h1 className="register__title">
-                Добро пожаловать!
-            </h1>
-            <form className="register__form">
-                <div className="register__form-top">
-                    <p className="register__name">Имя</p>
-                    <input id="register__input" type="text" name="register__input" className="register__input register__input_name" required minLength="2" maxLength="40"/>
-                    <span className="register__input-error">Что-то пошло не так...</span>
-                    <p className="register__name">E-mail</p>
-                    <input type="email" name="register__input" className="register__input register__input_email" required/>
-                    <span className="register__input-error">Что-то пошло не так...</span>
-                    <p className="register__name">Пароль</p>
-                    <input type="password" name="register__input" className="register__input register__input_password" required/>
-                    <span className="register__input-error">Что-то пошло не так...</span>
-                </div>
-                
-                <div className="register__form-bottom">
-                    <button type="submit" className="register__form-button">
-                        Зарегистрироваться
-                    </button>
-                    <div className="register__signin">
-                        <p className="register__signin-text">
-                            Уже зарегистрированы?
-                        </p>
-                        <Link to={"/signin"} className="register__signin-link">Войти</Link>
+    handleSubmit = evt => {
+        evt.preventDefault();
+    };
+
+    handleChange = evt => {
+        const fieldErrors = {
+          ...this.state.fieldErrors,
+          [evt.target.name]: ""
+        };
+    
+        this.setState({ fieldErrors });
+    };
+
+    handleInvalid = evt => {
+        evt.preventDefault();
+        const fieldErrors = {
+          ...this.state.fieldErrors,
+          [evt.target.name]: evt.target.validationMessage
+        };
+    
+        this.setState({ fieldErrors });
+    };
+
+    render() {
+        const { fieldErrors } = this.state;
+        return (
+            <section className="register">
+                <Link to={"/"} className="register__logo">
+                    <img src={logo} alt="Логотип" className="register__logo-image"/>
+                </Link>
+                <h1 className="register__title">
+                    Добро пожаловать!
+                </h1>
+                <form className="register__form"
+                    onSubmit={this.handleSubmit}
+                    onChange={this.handleChange}
+                    onInvalid={this.handleInvalid}
+                >
+                    <div className="register__form-top">
+                        <p className="register__name">Имя</p>
+                        <input 
+                            class={`register__input register__input_name ${fieldErrors.username ? "register__input_error-switch" : ""}`}
+                            type="text" 
+                            name="username"
+                            required 
+                            minLength="2" 
+                            maxLength="40"
+                        />
+                        { fieldErrors.username && (
+                            <span className="register__input-error">Что-то пошло не так...</span>
+                        )}
+                        <p className="register__name">E-mail</p>
+                        <input 
+                            class={`register__input register__input_email ${fieldErrors.email ? "register__input_error-switch" : ""}`}
+                            type="email" 
+                            name="email"
+                            required
+                        />
+                        { fieldErrors.email && (
+                            <span className="register__input-error">Что-то пошло не так...</span>
+                        )}
+                        <p className="register__name">Пароль</p>
+                        <input
+                            class={`register__input register__input_password ${fieldErrors.password ? "register__input_error-switch" : ""}`}
+                            type="password"
+                            name="password"
+                            required
+                        />
+                        { fieldErrors.password && (
+                            <span className="register__input-error">Что-то пошло не так...</span>
+                        )}
                     </div>
-                </div>
-            </form>
-        </section>
-    )
+                    
+                    <div className="register__form-bottom">
+                        <button type="submit" className="register__form-button">
+                            Зарегистрироваться
+                        </button>
+                        <div className="register__signin">
+                            <p className="register__signin-text">
+                                Уже зарегистрированы?
+                            </p>
+                            <Link to={"/signin"} className="register__signin-link">Войти</Link>
+                        </div>
+                    </div>
+                </form>
+            </section>
+        )
+    }
 }
-
-export default Register;
